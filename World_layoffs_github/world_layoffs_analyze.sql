d58edf4207d0e356eabb6d94b0acde9bbfd9ca0c
jaranda915 <aranda.j0312@gmail.com>
2024-09-22 20:00:55 -0500

    Update world_layoffs_analyze.sql

--- World_layoffs_github/world_layoffs_analyze.sql
+++ World_layoffs_github/world_layoffs_analyze.sql
@@ -12,7 +12,7 @@ order by total_laid_off desc;
 select company,sum(total_laid_off)
 from layoffs_copy2
 group by company
-order by 2 desc; #second column in select
+order by 2 desc;       #second column in select
 
 #ex looking at the time/date this data was collected from
 select min(`date`),max(`date`)
@@ -36,7 +36,7 @@ from layoffs_copy2
 group by year(`date`)
 order by 2 desc;
 
-#real: progression of layoffs by using a rolling sum based on month and year (rolling total essentially just constatnly adds the total_laid_off over each month)
+#Analysis 1: progression of layoffs by using a rolling sum based on month and year (rolling total essentially just constantly adds the total_laid_off over each month)
 with Rolling_Total as (
 select date_format(`date`,'%Y-%m') as `month`,sum(total_laid_off) as laid_off_total
 from layoffs_copy2
@@ -47,19 +47,19 @@ order by 1 asc
 select `month`, laid_off_total, sum(laid_off_total) over (order by `month`) as rolling_total #partition by not needed since cte table already grouped by month
 from Rolling_Total;
 
-#real, step1: showing how many layoffs each company did per year
+#Analysis 2, step1: showing how many layoffs each company did per year
 select company,year(`date`),sum(total_laid_off)
 from layoffs_copy2
 group by company,year(`date`)
 order by 1 asc; 
 
-#real, step2: rank the amount of laid off employees per year (showing what company laid off the most employees in each year by getting top 5 most emplyoee lay off for that year)
+#Analysis 2, step2: rank the amount of laid off employees per year (showing what company laid off the most employees in each year by getting top 5 most emplyoee lay off for that year)
 with Company_Year as (
 select company,year(`date`) as years,sum(total_laid_off) as laid_off_total
 from layoffs_copy2
 where year(`date`) IS NOT NULL
 group by company,years
-), Company_Year_Rank as ( #another cte table needed in order to use ranks below
+), Company_Year_Rank as (                   #another cte table needed in order to use ranks below
 select *, dense_rank() over(partition by years order by laid_off_total desc) as ranks
 from Company_Year
 )
@@ -67,11 +67,7 @@ select *
 from Company_Year_Rank
 where ranks<=5;
 
-/*
-The argument here is that a large company with a layoff proportion of 0.05 could still show more people being laid off, then a smaller company with a layoff proportion of 0.7, 
-even though this gives valuable insight that the past 3 years have hit small companies very hard
-*/
-#mine: shows layoff proportion. Shows which type of business got hit the hardest in proportion to their total employees (small companies got hit hardest, which is shown through seed: start of business.)
+#Analysis 3: shows layoff proportion. Shows which type of business got hit the hardest in proportion to their total employees (small companies got hit hardest, which is shown through "seed": start of business.)
 SELECT stage, ROUND(AVG(percentage_laid_off),2) as layoffs_per_stage
 FROM layoffs_copy2
 where stage IS NOT NULL
